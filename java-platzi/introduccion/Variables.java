5721736d5c22383c59d97c3c202368190f7a347f
rafaelpardor <rafaelpardor@protonmail.com>
2020-06-10 02:57:50 -0400

    Variables

---
+++ java-platzi/introduccion/Variables.java
@@ -0,0 +1,15 @@
+public class Variables{
+  public static void main(String[] args){
+    float gravity;
+    gravity = 9.8f;
+    System.out.println(gravity);
+
+    int salary = 2000;
+    String first_name = "Rafael";
+    System.out.println(first_name + " " + salary);
+
+    int bounty = 20;
+    salary = 3000 + bounty;
+    System.out.println(first_name + " " + salary);
+  }
+}
