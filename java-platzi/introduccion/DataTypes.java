8f71394f5447c1c0dde343be298a88f74c376c04
rafaelpardor <rafaelpardor@protonmail.com>
2020-06-10 14:47:18 -0400

    Data types

---
+++ java-platzi/introduccion/DataTypes.java
@@ -0,0 +1,21 @@
+public class DataTypes {
+  public static void main(String[] args){
+    // integers
+    // byte 1 byte -128 a 127
+    byte byteData = 2;
+    // short 2 bytes -32768 a 32757
+    // int 4 bytes
+    // long 8 bytes
+
+    // float numbers
+    // float 4 bytes
+    // double 8 bytes
+
+    // text
+    // char
+
+    // logical
+    // boolean 2 bytes
+
+  }
+}
