c43241bae359d23d87bbdaf2d34059216e796206
rafaelpardor <rafaelpardor@protonmail.com>
2020-06-10 21:54:19 -0400

    Arrays w for

--- java-platzi/introduccion/Arrays.java
+++ java-platzi/introduccion/Arrays.java
@@ -1,7 +1,7 @@
 public class Arrays{
 	public static void main(String[] args) {
 		String[] androidVersions = new String[]{"Apple Pie"};
-		System.out.println(Arrays.toString(androidVersions));
+		System.out.println(androidVersions[0]);
 
 		String days[] = new String[7];
 		days[0] = "Monday";
@@ -11,8 +11,24 @@ public class Arrays{
 		days[4] = "Friday";
 		days[5] = "Saturday";
 		days[6] = "Sunday";
-		for (int i = 0; i <= days.length-1; i++) {
-			System.out.printl(days[i]);
+		for (int i = 0; i < days.length; i++) {
+			System.out.println(days[i]);
+		}
+
+		System.out.println("Ciudades");
+		String[][] cities = new String[4][2];
+		cities[0][0] = "Colombia";
+		cities[0][1] = "Bogota";
+		cities[1][0] = "Colombia";
+		cities[1][1] = "Medallo";
+		cities[2][0] = "Colombia";
+		cities[2][1] = "Sta Marta";
+		cities[3][0] = "Colombia";
+		cities[3][1] = "Valledupar";
+		for (int i = 0; i < cities.length; i++){
+			for (int j = 0; j < cities[i].length; j++){
+				System.out.println(i + j + cities[i][j]);
+			}
 		}
 	}
 }
